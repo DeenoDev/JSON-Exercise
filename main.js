8e4fcde06ca9cba0fabc7dfa6264919127e1c13b
DeenoDev <deyanoklaasen@outlook.com>
2022-11-17 23:12:52 +0200

    populate header function added and completed.

--- main.js
+++ main.js
@@ -10,3 +10,14 @@ async function populate() {
     populateHeroes(superHeroes);
   
   }
+
+  function populateHeader(obj) {
+    const header = document.querySelector('header');
+    const myH1 = document.createElement('h1');
+    myH1.textContent = obj.squadName;
+    header.appendChild(myH1);
+  
+    const myPara = document.createElement('p');
+    myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+    header.appendChild(myPara);
+  }
